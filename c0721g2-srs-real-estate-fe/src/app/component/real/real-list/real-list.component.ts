af11fb52fee62620687919f5f07714e1994a22b3
khaiphan101 <88071022+khaiphan101@users.noreply.github.com>
2022-01-12 23:26:37 +0700

    update xong search dia chi, phan search filter dang lam

--- c0721g2-srs-real-estate-fe/src/app/component/real/real-list/real-list.component.ts
+++ c0721g2-srs-real-estate-fe/src/app/component/real/real-list/real-list.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RealEstateNew} from "../../../model/real-estate-new/real-estate-new";
 import {RealService} from "../../../service/real.service";
 import {ActivatedRoute, ParamMap} from "@angular/router";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {RealEstateType} from "../../../model/real-estate-new/real-estate-type";
+
 
 @Component({
   selector: 'app-real-list',
@@ -13,15 +16,35 @@ export class RealListComponent implements OnInit {
   term: string;
   p: any;
   mess: string;
+  formSearch: FormGroup;
+
+  public realEstateTypes: RealEstateType[];
+
+  // C2: dung new formGroup
+  // formSearch = new FormGroup({
+  //   address : new FormControl()
+  // });
+  // get Address()
+  // {
+  //   return this.formSearch.get('address');
+  // }
 
   constructor(
+    private _formBuilder: FormBuilder,
     public  realService: RealService,
     public activatedRoute: ActivatedRoute,
-
   ) {
   }
 
   ngOnInit(): void {
+    this.realService.getAllRealEstateTypes().subscribe(data => {
+        this.realEstateTypes = data.content;
+        console.log(this.realEstateTypes);
+      }
+    );
+    this.formSearch = this._formBuilder.group({
+      address: [''],
+    });
     this.realService.getAllRealEstates().subscribe(data => {
         this.realEstateNews = data.content;
         console.log(this.realEstateNews);
@@ -30,6 +53,18 @@ export class RealListComponent implements OnInit {
     this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
       this.mess = paramMap.get('mess');
     });
+    // C2:
+    // this.formSearch = this._formBuilder.group({
+    //   address: [''],
+    // });
   }
 
+  onSearch(searchInfo) {
+    // nếu dùng newform thì this.formSearch.Adress
+    this.realService.getAllRealEstatesByAdress(this.formSearch.value.address).subscribe(data => {
+        this.realEstateNews = data.content;
+        console.log(this.realEstateNews);
+      }
+    );
+  }
 }
--- c0721g2-srs-real-estate-fe/src/app/service/real.service.ts
+++ c0721g2-srs-real-estate-fe/src/app/service/real.service.ts
@@ -11,6 +11,7 @@ export class RealService {
   private API_URL_HISTORY_POST = ' http://localhost:8080/api/real-estate-new/history-post';
   private API_URL_EMAIL = ' http://localhost:8080/api/real-estate-new/email';
   private API_URL_LIST = 'http://localhost:8080/list-real-estate-new/search';
+  private API_URL_REAL_ESTATE_TYPE = 'http://localhost:8080/dealEstateType';
 
   constructor(private httpClient: HttpClient) {
   }
@@ -23,7 +24,17 @@ export class RealService {
     return this.httpClient.get<RealEstateNew>
     (this.API_URL_HISTORY_POST + '?customerId=' + customerId + '&&title=' + name + '&&kindOfNew=' + realNewType);
   }
+
   getAllRealEstates(): Observable<any> {
     return this.httpClient.get(this.API_URL_LIST);
   }
+
+  getAllRealEstatesByAdress(address: string): Observable<any> {
+    return this.httpClient.get(this.API_URL_LIST + '?address=' + address);
+  }
+
+  getAllRealEstateTypes(): Observable<any>{
+    return this.httpClient.get(this.API_URL_REAL_ESTATE_TYPE);
+  }
+
 }
