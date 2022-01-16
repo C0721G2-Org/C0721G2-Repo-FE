e121cb497c8bb39910ef1809fc55c6a71dcb0719
hungnn88dn <hungnn88dn@gmail.com>
2022-01-16 12:03:50 +0700

    hungnn

--- c0721g2-srs-real-estate-fe/src/app/component/employee/employee-list/employee-list.component.ts
+++ c0721g2-srs-real-estate-fe/src/app/component/employee/employee-list/employee-list.component.ts
@@ -1,10 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Employee} from '../../../model/employee/employee';
 import {EmployeeService} from '../../../service/employee.service';
-import {FormControl, FormGroup} from '@angular/forms';
 import {EmployeeDeleteComponent} from '../employee-delete/employee-delete.component';
 import {MatDialog} from '@angular/material/dialog';
-import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-employee-list',
@@ -13,109 +12,110 @@ import {Router} from '@angular/router';
 })
 export class EmployeeListComponent implements OnInit {
   employees: Employee[];
-  indexPagination: number = 1;
-  totalPagination: number;
-  totalPaginationArray: number[] = [];
-  employeeForm: FormGroup;
-  listEmployeeNotPagination: Employee[] = [];
-  total: number;
+  private subscription: Subscription | undefined;
+  page = 0;
+  employeeName = '';
+  employeeEmail = '';
+  employeePosition = '';
+  totalPages: number;
+  pageNumber: number;
+  size = 0;
+  flag = false;
+  message: string;
 
   positionList = [{id: 1, name: 'Nhân Viên'}, {id: 2, name: 'Kế Toán'}, {id: 3, name: 'Chuyên Viên'},
     {id: 4, name: 'Trưởng Phòng'}, {id: 5, name: 'Giám Đốc'}]
   ;
 
-
   constructor(private employeeService: EmployeeService,
-              private dialogDelete: MatDialog, private router: Router) {
+              private dialogDelete: MatDialog) {
   }
 
   ngOnInit(): void {
-    this.employeeService.getAllEmployee(0).subscribe(data => {
-      this.employees = data.content;
-      console.log(data.content.valueOf()) ;
-    });
-    this.employeeForm = new FormGroup({
-      name: new FormControl(''),
-      email: new FormControl(''),
-      position: new FormControl(''),
-    });
-    this.employeeService.getAllEmployeeNoPage().subscribe(data => {
-      this.listEmployeeNotPagination = data;
-      console.log(this.listEmployeeNotPagination.length);
-      if (this.listEmployeeNotPagination.length !== 0) {
-        this.totalPagination = (Math.floor(this.listEmployeeNotPagination.length / 10)) + 1;
-      }
-      for (let i = 1; i <= this.totalPagination; i++) {
-        this.totalPaginationArray.push(i);
-      }
-      console.log(this.totalPaginationArray);
-    });
-  }
-
-  findPaginnation(i) {
-    this.employeeService.getAllEmployee(i).subscribe(data => {
-      this.employees = data.content;
-    });
-  }
-
-  indexPaginationChage(value: number) {
-    this.indexPagination = value;
-  }
-
-  firtPage() {
-    this.employeeService.getAllEmployee(0).subscribe(data => {
-      this.employees = data.content;
-    });
-  }
-
-  nextPage() {
-    this.indexPagination = this.indexPagination + 1;
-    if (this.indexPagination > this.totalPagination) {
-      this.indexPagination = this.indexPagination - 1;
-    }
-    this.employeeService.getAllEmployee(this.indexPagination).subscribe(data => {
-      this.employees = data.content;
-    });
-  }
-
-  previousPage() {
-    this.indexPagination = this.indexPagination - 1;
-    if (this.indexPagination === 0) {
-      this.indexPagination = 1;
-      this.ngOnInit();
-    } else {
-      this.employeeService.getAllEmployee(this.indexPagination).subscribe(data => {
-        this.employees = data.content;
-      });
-    }
-  }
-
-  lastPage() {
-    this.indexPagination = this.totalPaginationArray.length - 1;
-    this.employeeService.getAllEmployee(this.indexPagination).subscribe(data => {
-      this.employees = data.content;
-    });
+    this.search();
   }
 
   search() {
-    this.employeeService.search(this.employeeForm.value.name, this.employeeForm.value.email,
-      this.employeeForm.value.position
-    ).subscribe(data => {
-      this.employees = data.content;
-    });
+    if (this.employeeName === '' && this.employeeEmail === '' && this.employeePosition === '') {
+      this.flag = false;
+      this.employeeService.search(this.page, this.employeeName, this.employeeEmail, this.employeePosition).subscribe(data => {
+        console.log(data);
+        if (data !== null) {
+          this.employees = data.content;
+          this.totalPages = data.totalPages;
+          this.pageNumber = data.pageable.pageNumber;
+          this.size = data.size;
+          this.page = data.pageable.pageNumber;
+          this.message = '';
+        } else {
+          this.message = 'Not found !!!';
+        }
+      });
+    } else {
+      if (this.flag === false) {
+        this.page = 0;
+        this.employeeService.search(this.page, this.employeeName, this.employeeEmail, this.employeePosition).subscribe(data => {
+          if (data !== null) {
+            this.employees = data.content;
+            this.totalPages = data.totalPages;
+            this.pageNumber = data.pageable.pageNumber;
+            this.size = data.size;
+            this.page = data.pageable.pageNumber;
+            this.message = '';
+          } else {
+            this.message = 'Not found !!!';
+          }
+          this.flag = true;
+        });
+      } else {
+        this.employeeService.search(this.page, this.employeeName, this.employeeEmail, this.employeePosition).subscribe(data => {
+          if (data !== null) {
+            this.employees = data.content;
+            this.totalPages = data.totalPages;
+            this.pageNumber = data.pageable.pageNumber;
+            this.size = data.size;
+            this.page = data.pageable.pageNumber;
+            this.message = '';
+            console.log(this.message);
+          } else {
+            this.message = 'Not found !!!';
+          }
+          this.flag = true;
+        });
+      }
+
+    }
+  }
+
+  previousClick(index) {
+    this.page = this.page - index;
+    this.ngOnInit();
+  }
+
+  nextClick(index) {
+    this.page = this.page + index;
+    console.log('next pay ' + this.page);
+    this.ngOnInit();
+  }
+
+  findPaginnation(value: number) {
+    this.page = value - 1;
+    this.ngOnInit();
+  }
+
+  onsubmit() {
+    this.flag = false;
+    this.search();
   }
 
   openDialog(employeeId: string) {
     console.log(employeeId);
-    // Trả về đối tượng
     this.employeeService.getEmployee(employeeId).subscribe(customerData => {
       const dialogRef = this.dialogDelete.open(EmployeeDeleteComponent, {
         width: '500px',
         data: {customerData},
-        // Khi bấm ra ngoài dialog khong bi mat di
         disableClose: true
       });
-
       dialogRef.afterClosed().subscribe(result => {
         console.log('The dialog was closed');
         this.ngOnInit();
--- c0721g2-srs-real-estate-fe/src/app/service/employee.service.ts
+++ c0721g2-srs-real-estate-fe/src/app/service/employee.service.ts
@@ -13,35 +13,19 @@ export class EmployeeService {
   constructor(private http: HttpClient) {
   }
 
-  getAllEmployee(index: number): Observable<any> {
-    return this.http.get(this.API + '/list?page=' + index);
-  }
-
-  getAllEmployeeNoPage(): Observable<any> {
-    return this.http.get(this.API);
-  }
-
   getEmployee(id: string): Observable<any> {
     return this.http.get(this.API + '/detail/' + id);
   }
 
-  search(name: string, email: string, positon: string): Observable<any> {
-    return this.http.get(this.API + '/search?name=' + name + '&email=' + email + '&position_id=' + positon);
+  search(page: number, name: string, email: string, positon: string): Observable<any> {
+    return this.http.get(this.API + '/search?name=' + name + '&email=' + email + '&position_id=' + positon + '&page=' + page);
   }
 
   save(employee: any): Observable<any> {
     return this.http.post(this.API, employee);
   }
 
-  findEmployeeById(id: string): Observable<any> {
-    return this.http.get(this.API + '/' + id);
-  }
-
   deleteEmployeeById(id: string): Observable<any> {
     return this.http.delete(this.API + '/delete/' + id);
-  }
-
-  updateEmployee(id: string, employee: Employee): Observable<any> {
-    return this.http.put(this.API + '/' + id, employee);
   }
 }
