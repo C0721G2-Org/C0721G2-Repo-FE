dd791c9f34e37114aee937c72514e59817add7c8
TuThinh198 <tolagau.98@gmail.com>
2022-01-18 15:22:23 +0700

    18/01

--- c0721g2-srs-real-estate-fe/src/app/app-routing.module.ts
+++ c0721g2-srs-real-estate-fe/src/app/app-routing.module.ts
@@ -4,12 +4,6 @@ import {AuthGuard} from './helpers/auth.guard';
 import {RealListComponent} from './component/real/real-list/real-list.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
-  {path: '', component: RealListComponent},
-  {path: 'customer', loadChildren: () => import('./component/customer/customer.module').then(module => module.CustomerModule)},
-  {path: 'employee', loadChildren: () => import('./component/employee/employee.module').then(module => module.EmployeeModule)},
-  {path: 'real-estate-new', loadChildren: () => import('./component/real/real.module').then(module => module.RealModule)},
-=======
   {
     path: 'customer',
     loadChildren: () => import('./component/customer/customer.module').then(module => module.CustomerModule),
@@ -30,8 +24,9 @@ const routes: Routes = [
     path: 'security',
     loadChildren: () => import('./component/security/security.module').then(module => module.SecurityModule)
   },
->>>>>>> 9871c9d8079e42482d5444e1e867d767730fbf1c
+
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- c0721g2-srs-real-estate-fe/src/app/component/employee/employee-routing.module.ts
+++ c0721g2-srs-real-estate-fe/src/app/component/employee/employee-routing.module.ts
@@ -4,18 +4,10 @@ import {EmployeeListComponent} from './employee-list/employee-list.component';
 import {EmployeeCreateComponent} from './employee-create/employee-create.component';
 import {EmployeeEditComponent} from './employee-edit/employee-edit.component';
 import {EmployeeDeleteComponent} from './employee-delete/employee-delete.component';
-import {AuthGuard} from '../../helpers/auth.guard';
 
 
 const routes: Routes = [
-<<<<<<< HEAD
   {path: 'list', component: EmployeeListComponent},
-=======
-  {
-    path: 'list',
-    component: EmployeeListComponent
-  },
->>>>>>> 9871c9d8079e42482d5444e1e867d767730fbf1c
   {path: 'create', component: EmployeeCreateComponent},
   {path: 'edit/:id', component: EmployeeEditComponent},
   {path: 'delete/:id', component: EmployeeDeleteComponent},
