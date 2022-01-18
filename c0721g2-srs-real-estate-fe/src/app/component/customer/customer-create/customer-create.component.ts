4c1a075a3bc44d2d29e2fbd5fd5083b0839f675d
tungle9 <lequoctung9@gmail.com>
2022-01-18 20:38:29 +0700

    upgit

--- c0721g2-srs-real-estate-fe/src/app/component/customer/customer-create/customer-create.component.ts
+++ c0721g2-srs-real-estate-fe/src/app/component/customer/customer-create/customer-create.component.ts
@@ -31,14 +31,13 @@ export class CustomerCreateComponent implements OnInit {
       address: this.formBuilder.control('', Validators.required),
       idCard: ['', [Validators.required, Validators.pattern('^([0-9]{9,12})$')]],
       dateOfBirth: this.formBuilder.control('', Validators.required),
-      phoneNumber: ['', [Validators.required, Validators.pattern('^([0-9]{8,12})$')]],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{9,12}$')]],
       gender: [2, Validators.required],
-      password: ['', [Validators.required, Validators.min(6), Validators.max(200)]],
+      userName: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(100)]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(200)]],
       confirmPassword: ['', [Validators.required]],
-      userName: ['', [Validators.required, Validators.min(2), Validators.max(100)]],
-      // , Validators.pattern('^([0-9]{9})|([0-9]{12})$')
     }, {
-      validator: this.checkPassword('passWord', 'confirmPassword')
+      validator: this.checkPassword('password', 'confirmPassword')
     });
   }
 
